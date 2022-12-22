78c1fc4e862c2e460635c5f3ac16d3afd426b0ef
TENSAEA <tensaeaschalew26@gmail.com>
2022-12-22 14:57:20 -0800

    update

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,14 +1,3 @@
-/**
- * infinite_add - add 2 numbers together
- * @n1: text representation of 1st number to add
- * @n2: text representation of 2nd number to add
- * @r: pointer to buffer
- * @size_r: buffer size
- * Return: pointer to calling function
- */
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
 	int overflow = 0, i = 0, j = 0, digits = 0;
 	int val1 = 0, val2 = 0, temp_tot = 0;
 
