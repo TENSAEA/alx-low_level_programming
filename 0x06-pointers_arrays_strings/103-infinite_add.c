73ab25096e26ca734a926defe528bb862b3dcae8
TENSAEA <tensaeaschalew26@gmail.com>
2022-12-22 16:19:44 -0800

    ta

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -46,6 +46,4 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 		 num = *(n2 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
-	}	
-	
-:%s/\s\+\+$//e}
+	}	:%s/\s\+$//e	
