16f7907d87dffe2d231b17325faa2117e31b5aaa
TENSAEA <tensaeaschalew26@gmail.com>
2022-12-22 16:08:06 -0800

    t

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -4,13 +4,13 @@
  * infinite_add - C function that add two numbers together
  *in strings to a buffer.
  *Assumes there are only digits stored in the number strings.
- * @n1: text representation of 1st number to add
- * @n2: text representation of 2nd number to add
- * @r: pointer to buffer
- * @size_r: buffer size
- * Return: pointer to calling function
+ *@n1: text representation of 1st number to add
+ *@n2: text representation of 2nd number to add
+ *@r: pointer to buffer
+ *@size_r: buffer size
+ *Return:returns pointer to calling function
  */
-
+  
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 }
@@ -25,10 +25,9 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 *	  if r cannot store the sum - 0.
 */
 
-char *infinite_add(char *n1, char *n2, char *r, int r_index)
+char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
 	int num, tens = 0;
-		
 	for (; *n1 && *n2; n1--, n2--, r_index--)
 	{
 		num = (*n1 - '0') + (*n2 - '0');
@@ -36,7 +35,6 @@ char *infinite_add(char *n1, char *n2, char *r, int r_index)
 		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
 	}
-		
 	for (; *n1; n1--; r_index++)
 	{
 		num = *(n1 - '0') + tens;
